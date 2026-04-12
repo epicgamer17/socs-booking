87eae652de7199a50274ebab8588bf09c53f3bb9
Sophia <sophiahussain1972@gmail.com>
2026-04-12 17:09:38 -0400

    db info for ownermiddleware

---
+++ app/server/middleware/ownerMiddleware.js
@@ -0,0 +1,6 @@
+//info for owner middleware
+//our db is id, email, password, role 
+//role being student or owner which is assigned during registration
+//so u could smth do like checking if request came from someone who is owner
+//or however u want. 
+
