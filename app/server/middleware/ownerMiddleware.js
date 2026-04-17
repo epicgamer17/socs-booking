431d94b554900ab69530dc043354b0075e1060a7
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2026-04-16 21:07:21 -0400

    Done `ownerMiddleware.js`?

--- app/server/middleware/ownerMiddleware.js
+++ app/server/middleware/ownerMiddleware.js
@@ -4,3 +4,12 @@
 //so u could smth do like checking if request came from someone who is owner
 //or however u want. 
 
+const requireOwner = (req, res, next) => {
+    if (req.user.role !== "owner") {
+        // thomas: `error` or `message`?
+        return res.status(403).json({ message: "Owner access required" }); 
+    }
+    next();
+};
+
+module.exports = requireOwner;
