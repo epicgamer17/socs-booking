93cc3bb084a0031b1e6153c38e3a6a0b8775ca53
Sophia <sophiahussain1972@gmail.com>
2026-04-18 12:56:54 -0400

    removed extra comment

--- app/server/middleware/ownerMiddleware.js
+++ app/server/middleware/ownerMiddleware.js
@@ -1,8 +1,4 @@
-//info for owner middleware
-//our db is id, email, password, role 
-//role being student or owner which is assigned during registration
-//so u could smth do like checking if request came from someone who is owner
-//or however u want. 
+
 
 const requireOwner = (req, res, next) => {
     if (req.user.role !== "owner") {
