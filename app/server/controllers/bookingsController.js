0a592095330bdb0e6b67469c5d487feb120025f5
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2026-04-20 21:40:24 -0400

    .

--- app/server/controllers/bookingsController.js
+++ app/server/controllers/bookingsController.js
@@ -19,16 +19,31 @@ Also, I think each slot should only have one booking, so we might want UNIQUE(sl
 exports.bookSlot = async (req, res) => {
     const userID = req.user.id;
     const slotID = req.params.id;
-    const dateBooked = req.body.date;
 
     try {
-        //insert slot into db
+        // verify slot exists
+        const [rows] = await db.query(
+            `SELECT slots.id
+             FROM slots
+             WHERE slots.id = ? AND isActive = TRUE`,
+            [slotID]
+        );
+        if (rows.length === 0) {
+            return res.status(404).json({ error: "Slot does not exist or is not active" });
+        }
+
+        // book the slot
+        // created at date already set automatically by the DB
         await db.query(
-            "INSERT INTO bookings (bookingID, slotID, userID, dateBooked) VALUES(?,?,?,?)",
-            [bookingID, slotID, userID, dateBooked]
+            "INSERT INTO bookings (slotID, userID) VALUES(?,?,?)",
+            [slotID, userID, dateBooked]
         );
         return res.status(201).json({ message: "Slot booked" });
     } catch (err) {
+        if (err.code === "ER_DUP_ENTRY") {
+            // needs UNIQUE(slotID) for this branch to ever trigger
+            return res.status(409).json({ message: "Slot already booked" });
+        }
         return res.status(500).json({ message: "Slot booking failed", error: err.message });
     }
 }
