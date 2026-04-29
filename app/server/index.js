9192df186e0fe93e6dc7757e94ab9f20762e365d
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2026-04-28 20:38:07 -0400

    Testing this rate limiting thing.

--- app/server/index.js
+++ app/server/index.js
@@ -12,6 +12,24 @@ const app = express();
 //jwt tokens can by read any anyone (which breaks the whole point of auth)
 
 
+// rate limiting
+// BEGIN: code pasted from https://www.npmjs.com/package/express-rate-limit
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+// END: code pasted from https://www.npmjs.com/package/express-rate-limit
+
+
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
